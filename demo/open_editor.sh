5c8c6a7930970a929d2d6a272a635efeaae7ebba
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2024-03-06 14:12:44 +0100

    Update open_editor.sh

--- demo/open_editor.sh
+++ demo/open_editor.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Open Vim in a new GNOME terminal window
-gnome-terminal -- vim temp &
+WIDTH=60
+HEIGHT=25
+XOFF=300
+YOFF=50
+
+# Open a new GNOME terminal window with specified geometry
+gnome-terminal --geometry=${WIDTH}x${HEIGHT}+${XOFF}+${YOFF} &
